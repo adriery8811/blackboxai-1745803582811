4e7d082d84c53a9aa51f3bc9117c86b390f5138b
adriery8811 <138053671+adriery8811@users.noreply.github.com>
2025-04-27 20:26:33 -0500

    Add maychi-app/frontend/src/theme.js 1745803591896

---
+++ maychi-app/frontend/src/theme.js
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    primary: '#222222',       // negro oscuro
+    accent: '#00d0ff',        // azul eléctrico
+    background: '#ffffff',    // blanco
+    text: '#333333',          // gris oscuro para texto
+    neutral: '#888888',       // gris medio
+  },
+  fonts: {
+    regular: 'Poppins-Regular',
+    bold: 'Poppins-Bold',
+  },
+};
